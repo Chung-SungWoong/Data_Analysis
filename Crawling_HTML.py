375d968dfd289d862cb5df1f070d8f858b60019f
Chung SungWoong <dnd8888@naver.com>
2022-11-22 20:32:34 +0900

    Data Search with tag

--- Crawling_HTML.py
+++ Crawling_HTML.py
@@ -13,5 +13,17 @@ ids_fruits1 = soup.select('#fruits1')               # 조건 뒤에 id 값에는
 class_price = soup.select('.price') 
 tags_span_class_price = soup.select('span.price')
 
+tags_name = soup.select('span.name')                # 태그만으로 정보를 찾기 어려울때 어떠한 부모 태그를 활용하여 찾을 수 있다.
+
+tags_banana = soup.select('#fruits1 > span.name')   # 위의 태그만으로는 파인애플과 바나나 둘 다 찾았지만, #fruits1이라는 조건을 더해줌으로써 banana만 나오게 되었다
+
+tags_banana2 = soup.select('div.sale > #fruits1 > span.name')
+
+tags_banana3 = soup.select('div.sale span.name')
+
+tags = soup.select('span.name')
+tag_1 = tags[0]
+
+
 
 
